c8ec6b58df5357628246bdda0a285cf03fbcbc67
Ghost <naclzfaklz@gmail.com>
2022-11-19 00:49:34 +0300

    lab4fix

--- lab4/test_lab3.py
+++ lab4/test_lab3.py
@@ -10,22 +10,26 @@ class lab3Test(unittest.TestCase):
         calories_calculator.add_record(r4)
         calories_calculator.add_record(r5)
         calories_calculator.add_record(r6)
-        print(calories_calculator.get_calories_remained())#Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 90 кКал
-        return calories_calculator.get_calories_remained()
+        #Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 1230 кКал
+        self.assertEqual(calories_calculator.get_calories_remained(), 'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 1230 кКал')
     
-    def test_CaloriesValues(test_CaloriesCalculator):
-        assert test_CaloriesCalculator, 'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более 90 кКал'
-
     def test_CashCalculator(self):
         cash_calculator = CashCalculator(1000)
         cash_calculator.add_record(r1)
         cash_calculator.add_record(r2)
         cash_calculator.add_record(r3)
-        print(cash_calculator.get_today_cash_remained('rub'))#Денег нет, держись: твой долг -1259.0 руб
-        return cash_calculator.get_today_cash_remained('rub')
+        #Денег нет, держись: твой долг -568.0 руб
+        self.assertEqual(cash_calculator.get_today_cash_remained('rub'), 'Денег нет, держись: твой долг -568.0 руб')
 
-    def test_CashValues(test_CashCalculator):
-        assert test_CashCalculator, 'Денег нет, держись: твой долг -1259.0 руб'  
+    def test_extra(self):
+        calories_calculator=CaloriesCalculator(1000)
+        calories_calculator.add_record(r4)
+        self.assertEqual(calories_calculator.get_today_limit_balance(), -186)
+
+    def test_currencies(self):
+        cash_calculator = CashCalculator(3000)
+        cash_calculator.add_record(r3)
+        self.assertRaises(TypeError,cash_calculator.get_today_cash_remained('юань'))
 
 if __name__ == '__main__' :
     unittest.main()       
