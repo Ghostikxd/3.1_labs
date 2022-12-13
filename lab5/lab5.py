7ccb3524c5e6f68be228908590dd626094374d31
Ghost <naclzfaklz@gmail.com>
2022-12-13 03:42:30 +0300

    lab5commit

---
+++ lab5/lab5.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
+#                     '$status $body_bytes_sent "$http_referer" '
+#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
+#                     '$request_time';
+
+
+import re
+import os
+import gzip
+import io
+from statistics import median
+
+config = {
+    "LOG_DIR": ".\log"
+}
+
+
+def main():
+    logs = {}
+    log_names = sorted(os.listdir(config.get('LOG_DIR')))
+    if not log_names:
+        return print(f'Logs not found')
+    for name in log_names:
+        if name == 'nginx-access-ui.log-20170630':
+            log_name = name
+    parsed_line = f'{config.get("LOG_DIR")}/{log_name}'
+    regex = re.compile(r'\"[A-Z]+ (\S+) .* (\d+\.\d+)\n')
+    total_count = 0
+
+    with open(parsed_line, 'r') as file:
+        for line in file:
+            result = regex.findall(line)
+            total_count += 1
+            if not len(result):
+                continue
+            url, time = result[0]
+            if url not in logs:
+                logs[url] = []
+            logs[url].append(float(time))
+
+        for key in logs:
+            count = len(logs[key])
+            print(f'URL: {key} \n'
+                  f'\tcount: {count}'
+                  f'\n\tcount_perc: {count/total_count * 100}'
+                  f'\n\ttime_avg: {sum(logs[key])/count}'
+                  f'\n\ttime_max: {max(logs[key])}'
+                  f'\n\ttime_median: {median(logs[key])}')
+            print('\n')
+
+
+main()
